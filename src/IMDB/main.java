cfcb226a972c9118b913c8db14fd3e67c331c87b
orasraf1241 <71230805+orasraf1241@users.noreply.github.com>
2021-11-02 16:30:51 +0200

    fixing syntax

--- src/IMDB/main.java
+++ src/IMDB/main.java
@@ -3,20 +3,17 @@ package IMDB;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Scanner;
-
-
 import java.io.IOException;
 
 public class main {
-
+     
      public static final String baseUrl = "https://www.imdb.com/find?s=tt&q=avengers&ref_=nv_sr_sm";
 
-     //in this fnc we check if the movie is exsist
      public static boolean isDevelopment(String url){
+         //in this fnc we check if the movie is exsist
          try {
              final Document document = Jsoup.connect(url).get();
              if (document.select("div.bAolrB.SubNav__SubNavContentBlock-sc-11106ua-2 > .ipc-link--launch.ipc-link--inherit-color.ipc-link--baseAlt.ipc-link").text().contains("In development: More at IMDbPro"))
@@ -33,8 +30,10 @@ public class main {
      }
 
      public static String movieUrl(StringBuilder inspect, String url, Document d,int i){
+          //in this methos we take the url 
          String moviaId = inspect.substring(inspect.indexOf("/title/"), inspect.lastIndexOf("?ref_=f"));
          String movieUrl = url + moviaId;
+          
          return d.select("tr.even.findResult:nth-of-type("+(i+1)+") > .result_text").text();
      }
      //this functions give as the first movie that hava all the rules
@@ -43,27 +42,28 @@ public class main {
          String searchResult = "https://www.imdb.com/find?q="+ movieName+ "&s=tt&ttype=ft&ref_=fn_ft";
          String name = null;
          String movieUrl = null;
+          
          try {
              final Document document = Jsoup.connect(searchResult).get();
              //here we get the numbers of the search result in IMDB
              int numberOfMovieResult =document.select("tr.odd.findResult > .result_text").size()+document.select("tr.even.findResult > .result_text").size();
-             //in this loop we check that we  have all the information we looking for about the movie
-             for (int i = 0; i < numberOfMovieResult; i++)
-              {
-
+            
+              //in this loop we check that we  have all the information we looking for about the movie
+             for (int i = 0; i < numberOfMovieResult; i++){
                  //building the url to a movie
                  if (i % 2 == 0) {
                      StringBuilder inspect = new StringBuilder(document.select("tr.odd.findResult:nth-of-type("+(i+1)+") > .result_text").toString());
                      String moviaId = inspect.substring(inspect.indexOf("/title/"), inspect.lastIndexOf("?ref_=f"));
                      movieUrl = url + moviaId;
                      name = document.select("tr.odd.findResult:nth-of-type("+(i+1)+") > .result_text").text();
+                
                  } else {
                      StringBuilder inspect = new StringBuilder(document.select("tr.even.findResult:nth-of-type("+(i+1)+") > .result_text").toString());
                      String moviaId = inspect.substring(inspect.indexOf("/title/"), inspect.lastIndexOf("?ref_=f"));
                      movieUrl = url + moviaId;
                      name = document.select("tr.even.findResult:nth-of-type("+(i+1)+") > .result_text").text();
                  }
-                 if (name.toLowerCase(Locale.ROOT).matches("(.*)"+movieName+"(.*)") || name.toUpperCase(Locale.ROOT).matches("(.*)"+movieName+"(.*)") || name.contains(movieName)){
+                   if (name.toLowerCase(Locale.ROOT).matches("(.*)"+movieName+"(.*)") || name.toUpperCase(Locale.ROOT).matches("(.*)"+movieName+"(.*)") || name.contains(movieName)){
                      if (isDevelopment(movieUrl)) {
                          Moviadetails m = new Moviadetails();
                          m.movieInfo(movieUrl, m);
@@ -79,14 +79,8 @@ public class main {
      }
 
 
-
-
-
-
      public static void main(String[] args) {
-
-
-
+          
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a Movia name: ");
@@ -96,15 +90,5 @@ public class main {
             System.out.println("We cant find your movie");
             return;
         }
-//        Moviadetails moviadetails = new Moviadetails();
-//        moviadetails.movieInfo(UrlMovie,moviadetails);
-
-
-       // moviadetails.toString();
-
-
-
-
      }
-
 }
